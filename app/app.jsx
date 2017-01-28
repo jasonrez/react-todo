8944103ca65fef696e476ad963f8e6f35b7590e2
jasonrez <jasonrez@hotmail.co.uk>
2017-01-28 16:08:16 +0000

    Create boiler plate 3

---
+++ app/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+
+
+$(document).foundation();
+
+//App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+<p>Boiler Plate 3 Project</p>,
+  document.getElementById('app')
+);
---
+++ app/tests/app.test.jsx
@@ -0,0 +1,10 @@
+// let expect = require('expect');
+import expect from 'expect';
+
+
+
+describe('App', () => {
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+});
