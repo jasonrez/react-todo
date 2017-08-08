5f79478971bc59b230b3a1604cb296fe66ab5c3e
jasonrez <jasonrez@hotmail.co.uk>
2017-08-08 22:28:14 +0100

    Add todosreducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,8 @@
+//let uuid = require('node-uuid');
+//let moment = require('moment');
+import uuid from 'node-uuid'
+import moment from 'moment'
+
 export let searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
@@ -10,7 +15,6 @@ export let searchTextReducer = (state = '', action) => {
 //showCompletedTodo default false, TOGGLE_SHOW_COMPLETED
 
 export let showCompletedReducer = (state = false, action)=> {
-  action.something = { holy: 'shit'}
   switch(action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
@@ -18,3 +22,34 @@ export let showCompletedReducer = (state = false, action)=> {
       return state;
   };
 };
+
+export let todoReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+    return [
+      ...state,
+      {
+        id: uuid(),
+        text: action.text,
+        createdAt: moment().unix(),
+        completedAt: undefined
+      }
+    ]
+    // add case for TOGGLE_TODO completed equal to opposite value & updateCompletedAt
+    case 'TOGGLE_TODO':
+
+      return state.map((todo)=>{
+        if(todo.id == action.id){
+          let nextCompleted  = !todo.completed
+
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt : nextCompleted ? moment().unix() : undefined
+          }
+        }
+      })
+    default:
+    return state
+  }
+}
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,7 @@
 import expect from 'expect';
 import df from 'deep-freeze-strict';
 import * as reducers from 'reducers'
+import moment from 'moment'
 
 describe('Reducers', () =>{
   describe('searchTextReducer', () => {
@@ -14,6 +15,7 @@ describe('Reducers', () =>{
       expect(res).toEqual(action.searchText);
     })
   })
+
   describe('showCompletedReducer', () =>{
     it('should flip status of showComplted', () =>{
       let action ={
@@ -22,7 +24,38 @@ describe('Reducers', () =>{
 
       let res = reducers.showCompletedReducer(df(false), df(action));
       expect(res).toEqual(true);
-
     })
   })
+
+  describe('todoReducer', () => {
+    it('should add new todo', () => {
+      let action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      let res = reducers.todoReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    })
+  })
+
+  describe('toggleRodo', () => {
+    it('should toggle todo', () => {
+      let todo = [{
+        id: 100,
+        completed: true,
+        completedAt: moment().unix()
+      }]
+      let action = {
+        type: 'TOGGLE_TODO',
+        id: 100
+      }
+      let res = reducers.todoReducer(df(todo),df(action))
+
+      expect(res[0].completed).toEqual(false)
+      expect(res[0].completedAt).toEqual(undefined)
+    })
+  })
+
 })
